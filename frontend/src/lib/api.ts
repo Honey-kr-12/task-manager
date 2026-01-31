630f0de31715ed71c7f3c6ad0f9eb5f294f34df8
Honey-kr <shivammahrolia@gmail.com>
2026-01-31 23:31:06 +0530

    done

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: "https://task-manager-jade-eight-36.vercel.app/api",
+  baseURL: "https://task-manager-backend-six-hazel.vercel.app//api",
 });
 
 api.interceptors.request.use((config) => {
